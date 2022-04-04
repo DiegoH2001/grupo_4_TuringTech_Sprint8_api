8ff16bd951b1cc0f4c148679cc8108f0bc52d32b
DiegoH2001 <diego20hidalgo20@gmail.com>
2022-04-04 12:36:23 -0300

    Api de Usuarios

--- src/controllers/api/productApiControllers.js
+++ src/controllers/api/productApiControllers.js
@@ -5,8 +5,16 @@ const sequelize = DB.sequelize;
 
 let productApiControllers = {
   listar: (req, res) => {
-    DB.Product.findAll().then((product) => res.json(product));
+    DB.Product
+    .findAll()
+    .then((products) => {
+      return res.status(200).json({
+        total: products.length,
+        data: products,
+        status: 200,
+      })
+    })
   },
-};
+}
 
 module.exports = productApiControllers;
