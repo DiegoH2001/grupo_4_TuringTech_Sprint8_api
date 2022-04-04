5caa218f7e66b58f40d6a82fc2a55e1dcf07487a
jessicabalbuena <jessbalbuenam@gmail.com>
2022-04-04 13:01:49 -0300

    product api controllers

--- src/controllers/api/productApiControllers.js
+++ src/controllers/api/productApiControllers.js
@@ -5,7 +5,69 @@ const sequelize = DB.sequelize;
 
 let productApiControllers = {
   listar: (req, res) => {
-    DB.Product.findAll().then((product) => res.json(product));
+    /**  DB.Product.findAll().then((products) => {
+      return res.status(200).json({
+        total: products.length,
+        data: products,
+        status: 200,
+      });
+    });
+*/
+    let equipos = products.count({ where: { categorie_id: 1 } });
+    let procesadoresCoolers = products.count({ where: { categorie_id: 2 } });
+    let motherboard = products.count({ where: { categorie_id: 3 } });
+    let memoriasRam = products.count({ where: { categorie_id: 4 } });
+    let placasDeVideo = products.count({ where: { categorie_id: 5 } });
+    let fuentesDeAlimentacion = products.count({ where: { categorie_id: 6 } });
+    let gabinetes = products.count({ where: { categorie_id: 7 } });
+    let perifericos = products.count({ where: { categorie_id: 8 } });
+    let accesorios = products.count({ where: { categorie_id: 9 } });
+
+    Promises.all[
+      (equipos,
+      procesadoresCoolers,
+      motherboard,
+      memoriasRam,
+      placasDeVideo,
+      fuentesDeAlimentacion,
+      gabinetes,
+      perifericos,
+      accesorios).then((categorie) => {
+        products
+          .findAll({ include: ["brand", "categorie", "discount"] })
+          .then((products) => {
+            let productsDetail = [];
+            products.forEach((product) => {
+              let productWithDetail = {
+                Product: product.name,
+                Id: product.id,
+                Description: product.product_description,
+                Price: product.price,
+                Image: product.sliced,
+                detail: `/api/productApi/${product.id}`,
+              };
+              productsDetail.push(productWithDetail);
+            });
+            let response = {
+              meta: { status: 200, url: "api/productApi/" },
+              totalProducts: products.length,
+              countByCategorie: [
+                { equipos: categorie[0] },
+                { procesadoresCoolers: categorie[1] },
+                { motherboard: categorie[2] },
+                { memoriasRam: categorie[3] },
+                { placasDeVideo: categorie[4] },
+                { fuentesDeAlimentacion: categorie[5] },
+                { gabinetes: categorie[6] },
+                { perifericos: categorie[7] },
+                { accesorios: categorie[8] },
+              ],
+              products: productsDetail,
+            };
+            res.json(response);
+          });
+      })
+    ];
   },
 };
 
