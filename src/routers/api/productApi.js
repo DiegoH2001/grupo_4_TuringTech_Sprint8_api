ff1e39ab7b9ff207457612b303ce60c77754e5d8
Facundo <facundodebenedetti09@hotmail.com>
2022-04-05 00:32:43 -0300

    ...

--- src/routers/api/productApi.js
+++ src/routers/api/productApi.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const apiController = require("../../controllers/api/productApiControllers");
 
 router.get("/listarProducto", apiController.listarProductos);
-router.get("/detalleProducto", apiController.detailProductos);
+router.get("/:id", apiController.detailProductos)
 
 module.exports = router;
