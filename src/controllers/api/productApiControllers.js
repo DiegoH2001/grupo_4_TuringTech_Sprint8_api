4880e4298dfae8b02e86a0b209e4f34983fa344f
Gabriel-mq <gabrielmakedas@gmail.com>
2022-04-05 00:31:59 -0300

    m

--- src/controllers/api/productApiControllers.js
+++ src/controllers/api/productApiControllers.js
@@ -16,79 +16,82 @@ let productApiControllers = {
       });
     });
 */
-    let equipos = products.count({ where: { categorie_id: 1 } });
-    let procesadoresCoolers = products.count({ where: { categorie_id: 2 } });
-    let motherboard = products.count({ where: { categorie_id: 3 } });
-    let memoriasRam = products.count({ where: { categorie_id: 4 } });
-    let placasDeVideo = products.count({ where: { categorie_id: 5 } });
-    let fuentesDeAlimentacion = products.count({ where: { categorie_id: 6 } });
-    let gabinetes = products.count({ where: { categorie_id: 7 } });
-    let perifericos = products.count({ where: { categorie_id: 8 } });
-    let accesorios = products.count({ where: { categorie_id: 9 } });
+    let equipos = products.count({ where: { categorie_id: 1 } })
+    let procesadoresCoolers = products.count({ where: { categorie_id: 2 } })
+    let motherboard = products.count({ where: { categorie_id: 3 } })
+    let memoriasRam = products.count({ where: { categorie_id: 4 } })
+    let placasDeVideo = products.count({ where: { categorie_id: 5 } })
+    let fuentesDeAlimentacion = products.count({ where: { categorie_id: 6 } })
+    let gabinetes = products.count({ where: { categorie_id: 7 } })
+    let perifericos = products.count({ where: { categorie_id: 8 } })
+    let accesorios = products.count({ where: { categorie_id: 9 } })
 
-    Promise.all([equipos,procesadoresCoolers,motherboard,memoriasRam,placasDeVideo,fuentesDeAlimentacion,gabinetes,perifericos,accesorios])
-      .then(categorie => {
+    Promise.all([
+      equipos,
+      procesadoresCoolers,
+      motherboard,
+      memoriasRam,
+      placasDeVideo,
+      fuentesDeAlimentacion,
+      gabinetes,
+      perifericos,
+      accesorios,
+    ]).then((categorie) => {
+      DB.Product.findAll({ include: ['brand', 'categorie', 'discount'] }).then(
+        (products) => {
+          let productsDetail = []
 
-        DB.Product.findAll({ include:["brand", "categorie", "discount"]})
-          .then(products => {
-
-            let productsDetail = [];
-
-            products.forEach((product) => {
-              let productWithDetail = {
-                Product: product.name,
-                Id: product.id,
-                Description: product.product_description,
-                Price: product.price,
-                Image: product.sliced,
-                detail: `/api/productApi/${product.id}`
-              };
-
-              productsDetail.push(productWithDetail);
-
-            });
-
-            let response = {
-              meta: { status: 200, url: "api/productApi/" },
-              totalProducts: products.length,
-              countByCategorie: [
-                { equipos: categorie[0] },
-                { procesadoresCoolers: categorie[1] },
-                { motherboard: categorie[2] },
-                { memoriasRam: categorie[3] },
-                { placasDeVideo: categorie[4] },
-                { fuentesDeAlimentacion: categorie[5] },
-                { gabinetes: categorie[6] },
-                { perifericos: categorie[7] },
-                { accesorios: categorie[8] }
-              ],
-              products: productsDetail,
-            };
-            res.json(response);
-          });
-      })
-    
-      
-    
-    },
-    detailProductos:(req,res) => {
-      DB.Product.findByPk(req.params.id,{include:["brand", "categorie", "discount"]
-      })
-      .then(product =>{
-            let response = {
-              meta: {
-                status:200,
-                
-                url: '/api/detalleproducto/:id'
-              },
-              data:{
-                id: product.id,
-                name: product.product_name
-              }
+          products.forEach((product) => {
+            let productWithDetail = {
+              Product: product.name,
+              Id: product.id,
+              Description: product.product_description,
+              Price: product.price,
+              Image: product.sliced,
+              detail: `/api/productApi/${product.id}`,
             }
 
-        res.json(response)
-      })
-    }
-  }
+            productsDetail.push(productWithDetail)
+          })
+
+          let response = {
+            meta: { status: 200, url: 'api/productApi/' },
+            totalProducts: products.length,
+            countByCategorie: [
+              { equipos: categorie[0] },
+              { procesadoresCoolers: categorie[1] },
+              { motherboard: categorie[2] },
+              { memoriasRam: categorie[3] },
+              { placasDeVideo: categorie[4] },
+              { fuentesDeAlimentacion: categorie[5] },
+              { gabinetes: categorie[6] },
+              { perifericos: categorie[7] },
+              { accesorios: categorie[8] },
+            ],
+            products: productsDetail,
+          }
+          res.json(response)
+        },
+      )
+    })
+  },
+  detailProductos: (req, res) => {
+    DB.Product.findByPk(req.params.id, {
+      include: ['brand', 'categorie', 'discount'],
+    }).then((product) => {
+      let response = {
+        meta: {
+          status: 200,
+          url: `/api/detalleproducto/:${product.id}`,
+        },
+        data: {
+          id: product.id,
+          name: product.product_name,
+        },
+      }
+
+      res.json(response)
+    })
+  },
+}
 module.exports = productApiControllers
--- src/routers/api/productApi.js
+++ src/routers/api/productApi.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const apiController = require("../../controllers/api/productApiControllers");
 
 router.get("/listarProducto", apiController.listarProductos);
-router.get("/detalleProducto", apiController.detailProductos);
+router.get("/detalleProducto/:id", apiController.detailProductos);
 
 module.exports = router;
