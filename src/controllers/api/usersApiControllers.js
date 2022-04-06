fb460e4743a42b1ce5c9eb2f5cf991bdee8cfe4c
Facundo <facundodebenedetti09@hotmail.com>
2022-04-05 23:04:56 -0300

    apis ok

--- src/controllers/api/usersApiControllers.js
+++ src/controllers/api/usersApiControllers.js
@@ -1,49 +1,67 @@
-const { count } = require('console')
-const path = require('path')
-const DB = require('../../database/models')
-const sequelize = DB.sequelize
+const { count } = require("console");
+const path = require("path");
+const DB = require("../../database/models");
+const sequelize = DB.sequelize;
+
 
 let usersApiControllers = {
-  listarUsuario: (req, res) => {
-    DB.User.findAll()
-      .then((usersDB) => {
-        let users = []
-        usersDB.forEach((user) => {
-          let userForEach = {
+    listarUsuario: (req, res) => {
+    DB.User
+    .findAll()
+    .then((usersDB) => {
+        let users = [];
+        usersDB.forEach(user => {
+            let userForEach = {
+                id: user.id,
+                name: user.user_fullname,
+                email: user.user_email,
+                detail:`api/usersApi/${user.id}` 
+
+            };
+            users.push(userForEach);
+        })
+        let respuesta = {
+            meta: {
+                status:200,
+                url:'api/usersApi'
+            },
+            count: users.length,
+            data: users
+        }
+        res.status(200).json(respuesta);
+    })
+    .catch(error =>
+        res.send(error))
+    },
+
+    detalleUsuario: (req,res) => {
+
+    DB.User.findByPk(req.params.id, {
+        include: ['rol']
+    })
+    .then(user => {
+        
+        let response = {
+            meta: {
+                status:200,
+                url: `/api/detalleusuario/:'${user.id}`,
+            },
+            data: {
             id: user.id,
             name: user.user_fullname,
             email: user.user_email,
-            detail: `api/usersApi/${user.id}`,
-          }
-          users.push(userForEach)
+            avatar: user.imageUser,
+            dni: user.user_dni,
+            cellphone: user.user_cellphone,
+            administrator: user.administrator
+        },
+        }           
+            
+            res.json(response);
         })
-        let respuesta = {
-          meta: {
-            status: 200,
-            url: 'api/usersApi',
-          },
-          count: users.length,
-          data: users,
-        }
-        res.status(200).json(respuesta)
-      })
-      .catch((error) => res.send(error))
-  },
-
-  detalleUsuario: (req, res) => {
-    DB.User.findByPk(req.params.id)
-      .then((user) => {
-        let usuario = {
-          id: user.id,
-          name: user.user_fullname,
-          email: user.user_email,
-          avatar: `/models/imageUser/${ImageUser.user_image_route}`,
-        }
-
-        res.status(200).json(usuario)
-      })
-      .catch((error) => res.send(error))
-  },
 }
+};
 
-module.exports = usersApiControllers
+
+module.exports = usersApiControllers;
+
